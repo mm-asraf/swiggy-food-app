167287f5f1da827e864de9f5cea8236c80df00cd
mm-asraf <mahboobasraf@gmail.com>
2023-06-02 15:24:47 +0530

    api setup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,59 @@
-import React, { useEffect } from 'react';
-import MainUI from '../UI/MainUI';
-import { API_URL } from '../Utils/api';
-import Card from '../UI/Card';
+import React, { useEffect, useState } from 'react';
+import MainUI from './UI/MainUI';
+import { API_URL, CLOUDINARY_URL } from '../Utils/api';
+import Card from './UI/Card';
+import './Main.css';
+
 const Main = () => {
+  const [restaurantData, setRestaurantData] = useState([]);
+
+  //making api calls
   const getRestaurantData = async () => {
     const res = await fetch(API_URL);
     const resData = await res.json();
 
-    console.log(resData.data.cards[0].data.data.cards);
+    setRestaurantData(resData.data.cards[0].data.data.cards);
   };
 
   useEffect(() => {
     getRestaurantData();
   }, []);
 
+  console.log('--------------------------------');
+  console.log(restaurantData);
+
   return (
     <MainUI>
-      <Card>card</Card>
+      <div className="container">
+        {restaurantData.map((res) => {
+          const {
+            name,
+            cloudinaryImageId,
+            cuisines,
+            costForTwoString,
+            avgRating,
+            deliveryTime,
+          } = res?.data;
+          return (
+            <Card key={res.id}>
+              <img
+                src={CLOUDINARY_URL + cloudinaryImageId}
+                alt="restaurant_img"
+                className="card_image"
+              />
+              <p className="card_name">{name}</p>
+              <p className="p card_cuisines">{cuisines.join(',')}</p>
+              <div className="set_in_one_line">
+                <span className="p card_costForTwoString">
+                  {costForTwoString}
+                </span>
+                <span className="p card_Rating">{avgRating}</span>
+                <span className="p card_deliveryTime">{deliveryTime}</span>
+              </div>
+            </Card>
+          );
+        })}
+      </div>
     </MainUI>
   );
 };
